5cce879feea923b4d57c6c22826a58635ce44ee0
John Turkson <git@johnturkson.com>
2020-11-14 16:42:25 -0800

    Publish packages to Space

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,14 +22,23 @@ subprojects {
                 name = "GitHubPackages"
                 url = uri("https://maven.pkg.github.com/johnturkson/aws-tools")
                 credentials {
-                    username = System.getenv("USERNAME")
-                    password = System.getenv("TOKEN")
+                    username = System.getenv("GITHUB_USERNAME")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+            
+            maven {
+                name = "SpacePackages"
+                url = uri("https://maven.pkg.jetbrains.space/johnturkson/p/aws/aws-tools")
+                credentials {
+                    username = System.getenv("SPACE_USERNAME")
+                    password = System.getenv("SPACE_TOKEN")
                 }
             }
         }
         
         publications {
-            register<MavenPublication>("github") {
+            create<MavenPublication>("maven") {
                 afterEvaluate {
                     from(components["kotlin"])
                 }
