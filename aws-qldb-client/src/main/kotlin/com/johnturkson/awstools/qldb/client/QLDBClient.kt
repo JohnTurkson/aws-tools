812c641af449ba6c0ac2ce426ca85f875e8864c1
John Turkson <git@johnturkson.com>
2020-12-10 06:12:01 -0800

    Update QLDB client data types

--- aws-qldb-client/src/main/kotlin/com/johnturkson/awstools/qldb/client/QLDBClient.kt
+++ aws-qldb-client/src/main/kotlin/com/johnturkson/awstools/qldb/client/QLDBClient.kt
@@ -2,13 +2,18 @@ package com.johnturkson.awstools.qldb.client
 
 import com.johnturkson.awstools.client.AWSClient
 import com.johnturkson.awstools.client.configuration.AWSCredentials
-import com.johnturkson.awstools.qldb.actions.StartSession
 import com.johnturkson.awstools.qldb.configuration.QLDBConfiguration
+import com.johnturkson.awstools.qldb.data.StartSessionRequestData
+import com.johnturkson.awstools.qldb.data.StartTransactionRequestData
+import com.johnturkson.awstools.qldb.errors.QLDBSessionException
 import com.johnturkson.awstools.qldb.requests.StartSessionRequest
+import com.johnturkson.awstools.qldb.requests.StartTransactionRequest
 import com.johnturkson.awstools.qldb.responses.StartSessionResponse
+import com.johnturkson.awstools.qldb.responses.StartTransactionResponse
 import com.johnturkson.awstools.requestsigner.AWSRequestSigner.Header
 import io.ktor.client.*
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.serializer
 
 class QLDBClient(
     credentials: AWSCredentials? = null,
@@ -18,15 +23,25 @@ class QLDBClient(
 ) : AWSClient(credentials, region, client, serializer) {
     
     suspend fun startSession(ledgerName: String): StartSessionResponse {
+        val request = StartSessionRequest(StartSessionRequestData(ledgerName))
+        return sendCommand(request)
+    }
+    
+    suspend fun startTransaction(ledgerName: String): StartTransactionResponse {
+        val sessionToken = startSession(ledgerName).startSession.sessionToken
+        val request = StartTransactionRequest(sessionToken, StartTransactionRequestData)
+        return sendCommand(request)
+    }
+    
+    private suspend inline fun <reified T, reified R> sendCommand(request: T): R {
         val target = "QLDBSession.SendCommand"
         val configuration = QLDBConfiguration(region)
-        val request = StartSession(StartSessionRequest(ledgerName))
-        val body = serializer.encodeToString(StartSession.serializer(), request)
+        val body = serializer.encodeToString(serializer(), request)
         val headers = generateHeaders(target, body)
         val (statusCode, responseBody) = makeRequest(configuration, body, headers)
         return when (statusCode) {
-            200 -> serializer.decodeFromString(StartSessionResponse.serializer(), responseBody)
-            else -> throw Exception()
+            200 -> serializer.decodeFromString(serializer(), responseBody)
+            else -> throw serializer.decodeFromString(serializer<QLDBSessionException>(), responseBody)
         }
     }
     
---
+++ aws-qldb-client/src/main/kotlin/com/johnturkson/awstools/qldb/errors/QLDBSessionException.kt
@@ -0,0 +1,30 @@
+package com.johnturkson.awstools.qldb.errors
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class QLDBSessionException : Exception() {
+    @SerialName("Message")
+    abstract override val message: String
+    
+    @Serializable
+    @SerialName("com.amazonaws.qldb.session#BadRequestException")
+    data class BadRequestException(@SerialName("Message") override val message: String) : QLDBSessionException()
+    
+    @Serializable
+    @SerialName("com.amazonaws.qldb.session#InvalidSessionException")
+    data class InvalidSessionException(@SerialName("Message") override val message: String) : QLDBSessionException()
+    
+    @Serializable
+    @SerialName("com.amazonaws.qldb.session#LimitExceededException")
+    data class LimitExceededException(@SerialName("Message") override val message: String) : QLDBSessionException()
+    
+    @Serializable
+    @SerialName("com.amazonaws.qldb.session#OccConflictException")
+    data class OccConflictException(@SerialName("Message") override val message: String) : QLDBSessionException()
+    
+    @Serializable
+    @SerialName("com.amazonaws.qldb.session#RateExceededException")
+    data class RateExceededException(@SerialName("Message") override val message: String) : QLDBSessionException()
+}
