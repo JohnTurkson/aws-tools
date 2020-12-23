fe551f6f27face13439457d9bbda64403c483af3
John Turkson <git@johnturkson.com>
2020-12-23 08:10:19 -0800

    Update ktor version

--- aws-qldb-client/build.gradle.kts
+++ aws-qldb-client/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-    implementation("io.ktor:ktor-client-core:1.4.3")
+    implementation("io.ktor:ktor-client-core:1.5.0")
     implementation(project(":aws-request-signer"))
     implementation(project(":aws-client"))
 }
