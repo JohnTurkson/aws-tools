a925b75f2ad6ad3f9fa60a215c8d2c41be979bad
John Turkson <git@johnturkson.com>
2021-06-20 23:45:30 -0700

    Update lambda client config

--- aws-lambda-client/build.gradle.kts
+++ aws-lambda-client/build.gradle.kts
@@ -5,9 +5,6 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-    implementation("io.ktor:ktor-client-core:1.5.0")
-    implementation("com.amazonaws:aws-lambda-java-core:1.2.1")
-    implementation(project(":aws-request-signer"))
-    implementation(project(":aws-client"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+    api("com.amazonaws:aws-lambda-java-core:1.2.1")
 }
